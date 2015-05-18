bc8f0ea8c0c0107d1ba6a9a2b2bbdcd6ac5af7c6
Alberto Frappa <frappa.a@gmail.com>
2015-05-19 00:17:50 +0100

    Created MVC project with default website template with Individual User Account authentication

---
+++ MachineLogViewer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MachineLogViewer.Startup))]
+namespace MachineLogViewer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
