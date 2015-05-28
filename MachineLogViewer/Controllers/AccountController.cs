35dc5e203d02b6bda70ebc32b671a95e81d2ef3e
Alberto Frappa <frappa.a@gmail.com>
2015-05-29 00:31:44 +0100

    Removed 'Remember me' feature

--- MachineLogViewer/Controllers/AccountController.cs
+++ MachineLogViewer/Controllers/AccountController.cs
@@ -76,7 +76,7 @@ namespace MachineLogViewer.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: true);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: false, shouldLockout: true);
             if (result == SignInStatus.Success)
             {
                 var db = new ApplicationDbContext();
